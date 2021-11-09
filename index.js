39ea1e50f7c852b82f9553dc38caa37e38ff6cd6
unknown <hshanto707@gmail.com>
2021-11-10 01:36:30 +0600

    cars api created

--- index.js
+++ index.js
@@ -10,22 +10,73 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
-    try {
-        await client.connect();
-        console.log('database connected');
-    }
-    finally {
-        // await client.close()
-    }
+  try {
+    await client.connect();
+    const database = client.db("wildWestCars");
+    const carCollection = database.collection("cars");
+    const cartCollection = database.collection("cart");
+
+    //! GET ALL TOUR INFO
+
+    app.get("/cars", async (req, res) => {
+      const cursor = carCollection.find({});
+      const cars = await cursor.toArray();
+      res.send(cars);
+    });
+
+    // app.get("/cart", async (req, res) => {
+    //   const cursor = cartCollection.find({});
+    //   const cars = await cursor.toArray();
+    //   res.send(cars);
+    // });
+
+    //! GET SINGLE TOUR INFO
+
+    // app.get("/cars/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log("getting specific tour", id);
+    //   const query = { _id: ObjectId(id) };
+    //   const tour = await carCollection.findOne(query);
+    //   res.json(tour);
+    // });
+
+    //! ADD DATA TO CART
+
+    // app.post("/cart", async (req, res) => {
+    //   const tour = req.body;
+    //   const result = await cartCollection.insertOne(tour);
+    //   res.json(result, 'hello cart');
+    // });
+
+    //! GET CART DATA
+
+    // app.get("/cart/:uid", async (req, res) => {
+    //   const uid = req.params.uid;
+    //   const query = { uid: uid };
+    //   const result = await cartCollection.find(query).toArray();
+    //   res.json(result);
+    // });
+
+    //! DELETE DATA FROM CART
+
+    // app.delete("/delete/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await cartCollection.deleteOne(query);
+    //   res.json(result);
+    // });
+  }
+  finally {
+    // await client.close()
+  }
 
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('server is running!')
+  res.send('server is running!')
 })
 
 app.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`)
+  console.log(`server running at http://localhost:${port}`)
 })
