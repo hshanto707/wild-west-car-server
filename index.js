c9b37b627494d592be4c986caa4756a170d4fcbf
unknown <hshanto707@gmail.com>
2021-11-11 20:46:48 +0600

    make admin added

--- index.js
+++ index.js
@@ -19,40 +19,43 @@ async function run() {
     const database = client.db("wildWestCars");
     const carCollection = database.collection("cars");
     const cartCollection = database.collection("cart");
+    const usersCollection = database.collection("users");
 
-    //! GET ALL TOUR INFO
+    // GET ALL CAR INFO
 
     app.get("/cars", async (req, res) => {
       const cursor = carCollection.find({});
-      const cars = await cursor.toArray();
-      res.send(cars);
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    // app.get("/cart", async (req, res) => {
-    //   const cursor = cartCollection.find({});
-    //   const cars = await cursor.toArray();
-    //   res.send(cars);
-    // });
-
-    //! GET SINGLE CAR INFO
+    // GET SINGLE CAR INFO
 
     app.get("/cars/:id", async (req, res) => {
       const id = req.params.id;
       console.log("getting specific tour", id);
       const query = { _id: ObjectId(id) };
-      const car = await carCollection.findOne(query);
-      res.json(car);
+      const result = await carCollection.findOne(query);
+      res.json(result);
     });
 
-    //! ADD DATA TO CART
+    // GET ALL CART INFO
 
-    // app.post("/cart", async (req, res) => {
-    //   const tour = req.body;
-    //   const result = await cartCollection.insertOne(tour);
-    //   res.json(result, 'hello cart');
-    // });
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    //! GET CART DATA
+    // ADD DATA TO CART
+
+    app.post("/cart", async (req, res) => {
+      const tour = req.body;
+      const result = await cartCollection.insertOne(tour);
+      res.json(result);
+    });
+
+    //! GET USER'S CART DATA
 
     // app.get("/cart/:uid", async (req, res) => {
     //   const uid = req.params.uid;
@@ -69,6 +72,50 @@ async function run() {
     //   const result = await cartCollection.deleteOne(query);
     //   res.json(result);
     // });
+
+    //post user to usersCollection
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    //findOut unique email
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //make admin a user
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      // console.log('put',user);
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+      });
+
+      //get user through email
+      app.get("/users/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        // console.log(user);
+        let isAdmin = false;
+        if (user?.role === "admin") {
+          isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+      });
   }
   finally {
     // await client.close()
